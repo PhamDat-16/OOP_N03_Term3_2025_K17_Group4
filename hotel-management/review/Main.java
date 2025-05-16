43330a4e8f1d2a4e26ab7ab3af37c9ec8a2577c7
PhamDat-16 <phvdat220805@gmail.com>
2025-05-16 02:10:29 +0000

    asdasd

---
+++ hotel-management/review/Main.java
@@ -0,0 +1,12 @@
+mport Test.TestUser;
+import Test.TestRecursion;
+import Test.TestTime;
+public class Main {
+    public static void' main(String[] args) throws InterruptedException {
+        TestUser.Usertest();
+        TestTime.TimeTest();
+        TestRecursion.RecursionTest();
+
+
+    }
+}
---
+++ hotel-management/review/TestRecursion.java
@@ -0,0 +1,10 @@
+
+public class TestRecursion {
+    public static void RecursionTest() {
+        Recursion r = new Recursion();
+
+        int n = 5;
+        System.out.println("Factorial of " + n + " is: " + r.factorial(n));
+        System.out.println("Fibonacci of " + n + " is: " + r.fibonacci(n));
+    }
+}
