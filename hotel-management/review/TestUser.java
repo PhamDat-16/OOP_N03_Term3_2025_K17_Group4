060227e3cdd28c9f9b52210d12a8be9ef5ce2d67
Nguyen Duy Bao <151889054+29042005123@users.noreply.github.com>
2025-05-16 02:07:52 +0000

    test

---
+++ hotel-management/review/TestUser.java
@@ -0,0 +1,7 @@
+public class TestUser {
+    public static void Usertest() {
+         User user = new User("Nhat", "Nhat965@gmail.com", "03456999567");
+         System.out.println("Name: " + user.getName());
+         System.out.println("Email: " + user.getEmail());
+         System.out.println("Phone: " + user.getPhone()); } 
+}
