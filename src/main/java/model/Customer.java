2d2e9385556c2083e7a6a8ef26ae2deb5463e0ba
PhamDat-16 <phvdat220805@gmail.com>
2025-05-30 02:30:00 +0000

    code day du pham dat

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,23 +1,41 @@
 package main.java.model;
 
 public class Customer {
-    private String name;
-    private String idCard;
-    private String phone;
-    public Customer(String name, String idCard, String phone) {
-        this.name = name;
-        this.idCard = idCard;
-        this.phone = phone;
-    }
-    public String getName() {
-        return name;
+    private int customerId;
+    private String customerName;
+    private String customerPhone;
+    private String customerEmail;
+    private String customerAddress;
+    private String customerIdCard;
+    private String customerNationality;
+
+    public Customer(int customerId, String customerName , String customerPhone , String customerEmail , String customerAddress , String customerIdCard , String customerNationality) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+        this.customerEmail = customerEmail;
+        this.customerAddress = customerAddress;
+        this.customerIdCard = customerIdCard;
+        this.customerNationality = customerNationality;
     }
 
-    public String getIdCard() {
-        return idCard;
-    }
+    public int getId() { return customerId; }
+    public String getName() { return customerName; }
+    public String getPhone() { return customerPhone; }
+    public String getEmail() {return customerEmail;}
+    public String getAddress() {return customerAddress;}
+    public String getIdCard() {return customerIdCard;}
+    public String getNationality() {return customerNationality;}
 
-    public String getPhone() {
-        return phone;
+
+    @Override
+    public String toString() {
+        return "ID: " + customerId + 
+        ", Ten: " + customerName + 
+        // ", SDT: " + customerPhone + 
+        // "\n" + ", Email: "+ customerEmail + 
+        // ", Dia chi nha: " + customerAddress +
+        ", CCCD/CMND: " + customerIdCard +
+        ", Quoc Tich: "+ customerNationality;
     }
 }
